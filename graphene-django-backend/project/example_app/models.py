513eb0834818d068c0db25cc4065bffddc031370
Rona Chong <ronachong@gmail.com>
2017-09-12 20:39:13 -0700

    Set up first models in example_app and register to admin.

--- graphene-django-backend/project/example_app/models.py
+++ graphene-django-backend/project/example_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+class MangaSeries(models.Model):
+    title = models.CharField(max_length=1000)
+    author = models.ForeignKey(Author, related_name='manga_series')
+    genre = models.ForeignKey(Genre, related_name='manga_series')
+
+    def __str__(self):
+        return self.title
