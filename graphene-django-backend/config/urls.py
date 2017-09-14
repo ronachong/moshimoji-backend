d3a651df3a92dbb9258bf48eb62ec2c12ddbb5fc
Rona Chong <ronachong@gmail.com>
2017-09-14 14:35:14 -0700

    Set up first graphql types and schema; add endpoint for graphql.

--- graphene-django-backend/config/urls.py
+++ graphene-django-backend/config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from graphene_django.views import GraphQLView
+from project.example_app.graphene.schema import schema
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('project.example_app.urls')),
+    url(r'^', GraphQLView.as_view(graphiql=True, schema=schema)),
+    # url(r'^', include('project.example_app.urls')),
 ]
