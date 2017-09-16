6843b6f28cc961c2bfd2af7b4f989c04ae7f4d63
Rona Chong <ronachong@gmail.com>
2017-09-15 17:01:44 -0700

    Add filtering functionality to 'connection' fields.
    Changes include adding a node interface to the GraphQL objects and
    making all_* fields into connection fields with DjangoFilterCon-
    nectionField.
    
    Field resolvers no longer appear to be necessary for all_* fields with
    change, but are still necessary for graphene.Field fields (if omitted,
    GraphQL introspection shows that args like id, name, etc. are missing
    for graphene.Field fields).
    
    Examples followed:
    https://github.com/rmoorman/django-graphql-apollo-react-demo#add-apollo
    http://docs.graphene-python.org/projects/django/en/latest/tutorial-relay/
    https://gearheart.io/blog/using-graphql-with-django/

--- graphene-django-backend/project/example_app/graphene/schema.py
+++ graphene-django-backend/project/example_app/graphene/schema.py
@@ -1,7 +1,7 @@
 import graphene
 
 from graphene_django.types import DjangoObjectType
-# from graphene_django.filter import
+from graphene_django.filter.fields import DjangoFilterConnectionField
 
 from project.example_app.models import Genre, Author, MangaSeries
 
@@ -10,25 +10,31 @@ from project.example_app.models import Genre, Author, MangaSeries
 # you could also read about models under django
 
 ''' Object type definitions for GraphQL server '''
-class GenreType(DjangoObjectType):
+class GenreNode(DjangoObjectType):
     class Meta:
         model = Genre
+        filter_fields = {
+            'name': ['icontains', 'exact'],
+        }
         interfaces = (graphene.Node, )
 
 class AuthorType(DjangoObjectType):
     class Meta:
         model = Author
+        filter_fields = ['first_name', 'last_name']
         interfaces = (graphene.Node, )
 
 class MangaSeriesType(DjangoObjectType):
     class Meta:
         model = MangaSeries
+        filter_fields = ['title', 'author', 'genre']
         interfaces = (graphene.Node, )
 
 
-''' Query type definition for GraphQL server, with resolver defs for fields '''
+''' Query type definition for GraphQL server '''
 class Query(graphene.ObjectType):
-    genre = graphene.Field(GenreType,
+    # field definitions
+    genre = graphene.Field(GenreNode,
         id=graphene.Int(),
         name=graphene.String()
     )
@@ -42,10 +48,12 @@ class Query(graphene.ObjectType):
         title=graphene.String(),
     )
 
-    all_genres = graphene.List(GenreType)
-    all_authors = graphene.List(AuthorType)
-    all_manga_series = graphene.List(MangaSeriesType)
+    all_genres = DjangoFilterConnectionField(GenreNode)
+    all_authors = DjangoFilterConnectionField(AuthorType)
+    all_manga_series = DjangoFilterConnectionField(MangaSeriesType)
 
+    # field resolvers (resolvers for 'connection' fields skipped since
+    # DjangoFilterConnectionField supplies functionality)
     def resolve_genre(self, info, **kwargs):
         id = kwargs.get('id')
         name = kwargs.get('name')
@@ -56,13 +64,12 @@ class Query(graphene.ObjectType):
             return Genre.objects.get(name=name)
         return None
 
-    def resolve_all_genres(self, info, **kwargs):
-        return Genre.objects.all()
+    def resolve_author(self, info, **kwargs):
+        """TBI"""
+        pass
 
-    def resolve_all_authors(self, info, **kwargs):
-        return Author.objects.all()
-
-    def resolve_all_manga_series(self, info, **kwargs):
-        return MangaSeries.objects.all()
+    def resolve_manga_series(self, info, **kwargs):
+        """TBI"""
+        pass
 
 schema = graphene.Schema(query=Query)
--- graphene-django-backend/project/example_app/models.py
+++ graphene-django-backend/project/example_app/models.py
@@ -4,9 +4,17 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{} {}".join(first_name, last_name)
+
 
 class MangaSeries(models.Model):
     title = models.CharField(max_length=1000)
