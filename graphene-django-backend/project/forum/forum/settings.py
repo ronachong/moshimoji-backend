f85441725deda6f37fdc960100e10183d7318766
Rona Chong <ronachong@gmail.com>
2017-11-14 16:13:26 -0800

    Port Misago auth settings to project config.

--- graphene-django-backend/project/forum/forum/settings.py
+++ graphene-django-backend/project/forum/forum/settings.py
@@ -39,33 +39,6 @@ CACHES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        'OPTIONS': {
-            'user_attributes': ['username', 'email'],
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 7,
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-
-
 # Email configuration
 # https://docs.djangoproject.com/en/1.11/ref/settings/#email-backend
 
