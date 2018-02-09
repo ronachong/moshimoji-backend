9a97cc5b20b004ba6bc048a380ffe1501998673b
Rona Chong <ronachong@gmail.com>
2018-02-09 12:59:36 -0800

    Update Django db settings and create settings module for stage.
    I've iterated on the stack such that the following applies:
    in dev, the database is postgres on the localhost
    in stage, the database is postgres on a swarm task 'database'
    in prod, the database is an RDS instance on the same VPC
    
    I've updated the local and production settings accordingly and
    created a new setting module for stage.

--- graphene-django-backend/config/settings/production.py
+++ graphene-django-backend/config/settings/production.py
@@ -1,7 +1,10 @@
 from .base import *
 
+print("in prod config!")
+
 # TODO: change this to actual prod key later
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env(
     'DJANGO_SECRET_KEY',
     default='ddhue(=qe14tlx(#dk2&x6r#ky4pk4f6tx9mvnhfdx_*7v)r6'
