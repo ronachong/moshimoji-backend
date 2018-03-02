571f170972bb639c2b8cd8e6acdb441c1da17038
Rona Chong <ronachong@gmail.com>
2018-03-02 13:42:21 -0800

    Add Genre model.

--- graphene-django-backend/project/gql_platform/admin.py
+++ graphene-django-backend/project/gql_platform/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register models here.
 from project.gql_platform.models.content.social import UserStatus
+from project.gql_platform.models.content.serialization.comic import Genre
 
 admin.site.register(UserStatus)
+admin.site.register(Genre)
---
+++ graphene-django-backend/project/gql_platform/models/content/serialization/comic.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# from project.gql_platform.models.content.serialization import base
+
+class Genre(models.Model):
+    name = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+#
+#
+# class Serialization(base.Serialization):
+#     ''' attributes from base:
+#         - title
+#         - author
+#         check base class for more attributes or for changes
+#     '''
+#     genres = models.ManyToManyField(Genre)
+#
+# class Issue(base.Issue):
+#     ''' attributes from base:
+#         - title
+#         - publication_date
+#         - serialization
+#         check base class for more attributes or for changes
+#     '''
+#     print_publication_date = models.DateTimeField()
+#
+# class Page(models.Model):
+#     # how to make sure that each page has a sequential id?
+#     issue = models.ForeignKey(Issue)
